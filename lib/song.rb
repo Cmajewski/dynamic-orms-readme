9633a942a74e54fa4d3cf18df2e3fdbc89c721d8
Caitlin Majewski <cmajewski@wesleyan.edu>
2019-11-02 00:06:45 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,6 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
+require "pry"
 
 class Song
 
@@ -7,13 +8,14 @@ class Song
   def self.table_name
     self.to_s.downcase.pluralize
   end
-
+ #
   def self.column_names
-    DB[:conn].results_as_hash = true
+    DB[:conn].results_as_hash = true  
 
     sql = "pragma table_info('#{table_name}')"
 
     table_info = DB[:conn].execute(sql)
+    #binding.pry
     column_names = []
     table_info.each do |row|
       column_names << row["name"]
